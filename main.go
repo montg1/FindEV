81eb5b3e896130180d351eb3058ae02a3bebed3b
Kajee <poomrapee.kho@gmail.com>
2023-11-28 16:56:38 +0700

    fix lat lng

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/url"
 	"os"
@@ -16,7 +16,12 @@ const hereAPIKey = "g-gI_EzetmmNF8WDJNLXRu-hYIqtoRj8OiGtGZADXeM"
 var laLong [][]string
 
 func main() {
-	laLong1 := readCSV()
+	laLong1, err := readCSV()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
 	for i := 0; i < len(laLong1); i++ {
 		lat := laLong1[i][0]
 		lng := laLong1[i][1]
@@ -30,36 +35,69 @@ func main() {
 
 		fmt.Println("EV Charging Stations:")
 		for _, station := range evChargePoints.Items {
-			fmt.Printf("Name: %s, Latitude: %f, Longitude: %f\n", station.Title, station.Address.Position.Lat, station.Address.Position.Lng)
+			fmt.Printf("Name: %s, Latitude: %f, Longitude: %f\n", station.Title, station.Position.Lat, station.Position.Lng)
 		}
 	}
 }
 
-type EVChargePointResponse struct {
+type EVChargingStation struct {
 	Items []struct {
-		Title   string `json:"title"`
-		Address struct {
-			Label    string `json:"label"`
-			Country  string `json:"country"`
-			State    string `json:"state"`
-			County   string `json:"county"`
-			City     string `json:"city"`
-			District string `json:"district"`
-			Street   string `json:"street"`
-			Postal   string `json:"postalCode"`
-			Position struct {
-				Lat float64 `json:"lat"`
-				Lng float64 `json:"lng"`
-			} `json:"position"`
+		Title      string `json:"title"`
+		ID         string `json:"id"`
+		Language   string `json:"language"`
+		OntologyID string `json:"ontologyId"`
+		ResultType string `json:"resultType"`
+		Address    struct {
+			Label       string `json:"label"`
+			CountryCode string `json:"countryCode"`
+			CountryName string `json:"countryName"`
+			State       string `json:"state"`
+			CountyCode  string `json:"countyCode"`
+			County      string `json:"county"`
+			City        string `json:"city"`
+			District    string `json:"district"`
+			Street      string `json:"street"`
+			PostalCode  string `json:"postalCode"`
 		} `json:"address"`
+		Position struct {
+			Lat float64 `json:"lat"`
+			Lng float64 `json:"lng"`
+		} `json:"position"`
+		Access []struct {
+			Lat float64 `json:"lat"`
+			Lng float64 `json:"lng"`
+		} `json:"access"`
+		Distance   float64 `json:"distance"`
+		Categories []struct {
+			ID      string `json:"id"`
+			Name    string `json:"name"`
+			Primary bool   `json:"primary"`
+		} `json:"categories"`
+		Contacts []struct {
+			Phone []struct {
+				Value string `json:"value"`
+			} `json:"phone"`
+			WWW []struct {
+				Value string `json:"value"`
+			} `json:"www"`
+		} `json:"contacts"`
+		OpeningHours []struct {
+			Text       []string `json:"text"`
+			IsOpen     bool     `json:"isOpen"`
+			Structured []struct {
+				Start      string `json:"start"`
+				Duration   string `json:"duration"`
+				Recurrence string `json:"recurrence"`
+			} `json:"structured"`
+		} `json:"openingHours"`
 	} `json:"items"`
 }
 
-func getEVChargePoints(lat, lng, limit string) (*EVChargePointResponse, error) {
+func getEVChargePoints(lat, lng, limit string) (*EVChargingStation, error) {
 	baseURL := "https://discover.search.hereapi.com/v1/discover"
 	apiURL, err := url.Parse(baseURL)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to parse API URL: %w", err)
 	}
 
 	params := url.Values{}
@@ -72,28 +110,34 @@ func getEVChargePoints(lat, lng, limit string) (*EVChargePointResponse, error) {
 
 	resp, err := http.Get(apiURL.String())
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to make API request: %w", err)
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("API request failed with status code: %d", resp.StatusCode)
 	}
 
-	var evChargePoints EVChargePointResponse
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read API response body: %w", err)
+	}
+
+	//fmt.Println("Raw JSON Response:", string(body))
+
+	var evChargePoints EVChargingStation
 	err = json.Unmarshal(body, &evChargePoints)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to unmarshal JSON response: %w", err)
 	}
 
 	return &evChargePoints, nil
 }
 
-func readCSV() [][]string {
+func readCSV() ([][]string, error) {
 	file, err := os.Open("Wales.csv")
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("failed to open CSV file: %w", err)
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
@@ -103,5 +147,5 @@ func readCSV() [][]string {
 		listItem := item[9:11]
 		laLong = append(laLong, listItem)
 	}
-	return laLong
+	return laLong, nil
 }
