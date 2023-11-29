8b4d134e43ee9e768d9c42ccac159bf7d2cf6a48
Kajee <poomrapee.kho@gmail.com>
2023-11-29 18:02:45 +0700

    still cant fix supplier problem

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"time"
 )
 
 const hereAPIKey = "g-gI_EzetmmNF8WDJNLXRu-hYIqtoRj8OiGtGZADXeM"
@@ -36,7 +37,16 @@ func main() {
 		fmt.Println("EV Charging Stations:")
 		for _, station := range evChargePoints.Items {
 			fmt.Printf("Name: %s, Latitude: %f, Longitude: %f\n", station.Title, station.Position.Lat, station.Position.Lng)
+			if len(station.Connectors.Connector) > 0 {
+				for _, connector := range station.Connectors.Connector {
+					fmt.Printf("  Supplier: %s, Charge Capacity: %s\n", connector.SupplierName, connector.ChargeCapacity)
+				}
+			} else {
+				fmt.Println("  No connectors available.")
+			}
+			fmt.Println("-------------------------------")
 		}
+
 	}
 }
 
@@ -90,6 +100,107 @@ type EVChargingStation struct {
 				Recurrence string `json:"recurrence"`
 			} `json:"structured"`
 		} `json:"openingHours"`
+		PoolID                  string `json:"poolId,omitempty"`
+		CpoID                   string `json:"cpoId,omitempty"`
+		TotalNumberOfConnectors int    `json:"totalNumberOfConnectors,omitempty"`
+		Connectors              struct {
+			Connector []struct {
+				SupplierName  string `json:"supplierName"`
+				ConnectorType []struct {
+					Name string `json:"name"`
+					ID   string `json:"id"`
+				} `json:"connectorType"`
+				ChargeCapacity        string  `json:"chargeCapacity"`
+				MaxPowerLevel         float64 `json:"maxPowerLevel"`
+				CustomerChargeLevel   string  `json:"customerChargeLevel"`
+				CustomerConnectorName string  `json:"customerConnectorName"`
+				FixedCable            bool    `json:"fixedCable"`
+				ConnectorDetails      struct {
+					PrivateAccess bool   `json:"privateAccess"`
+					Open24x7      bool   `json:"open24x7"`
+					OpeningTime   string `json:"openingTime"`
+					OpeningHours  struct {
+						RegularOpeningHours []struct {
+							Daymask []int `json:"daymask"`
+							Period  []struct {
+								From string `json:"from"`
+								To   string `json:"to"`
+							} `json:"period"`
+						} `json:"regularOpeningHours"`
+						AnnualOpenings []interface{} `json:"annualOpenings"`
+					} `json:"openingHours"`
+					Pay          bool   `json:"pay"`
+					Manufacturer string `json:"manufacturer"`
+				} `json:"connectorDetails"`
+				ChargingPoint []struct {
+					ChargeMode          string    `json:"chargeMode"`
+					VoltsRange          string    `json:"voltsRange"`
+					Phases              int       `json:"phases"`
+					AmpsRange           string    `json:"ampsRange"`
+					NumberOfConnectors  int       `json:"numberOfConnectors"`
+					NumberOfAvailable   int       `json:"numberOfAvailable"`
+					LastUpdateTimestamp time.Time `json:"lastUpdateTimestamp"`
+				} `json:"chargingPoint"`
+				ConnectorStatuses struct {
+					ConnectorStatus []struct {
+						CpoEvseId         string `json:"cpoEvseId"`
+						CpoEvseEMI3Id     string `json:"cpoEvseEMI3Id"`
+						PhysicalReference string `json:"physicalReference"`
+						CpoConnectorId    string `json:"cpoConnectorId"`
+						State             string `json:"state"`
+					} `json:"connectorStatus"`
+				} `json:"connectorStatuses"`
+			} `json:"connector,omitempty"`
+		} `json:"connectors,omitempty"`
+		EVStationDetails []struct {
+			PrivateAccess     bool   `json:"privateAccess"`
+			RestrictedAccess  bool   `json:"restrictedAccess"`
+			AccessibilityType string `json:"accessibilityType"`
+			PaymentMethods    struct {
+				Subscription struct {
+					Provider string `json:"provider"`
+					Accept   bool   `json:"accept"`
+				} `json:"subscription"`
+				Note       string `json:"note"`
+				CreditCard struct {
+					Types struct {
+						Type []string `json:"type"`
+					} `json:"types"`
+					Accept bool `json:"accept"`
+				} `json:"creditCard"`
+				Cash struct {
+					Currencies struct {
+						Currency []string `json:"currency"`
+					} `json:"currencies"`
+					Accept bool `json:"accept"`
+				} `json:"cash"`
+				DebitCard struct {
+					Accept bool `json:"accept"`
+				} `json:"debitCard"`
+				Check struct {
+					Types struct {
+						Type []string `json:"type"`
+					} `json:"types"`
+					Accept bool `json:"accept"`
+				} `json:"check"`
+				EPayment struct {
+					Types struct {
+						Type []string `json:"type"`
+					} `json:"types"`
+					Accept bool `json:"accept"`
+				} `json:"ePayment"`
+				Other struct {
+					Types struct {
+						Type []string `json:"type"`
+					} `json:"types"`
+					Accept bool `json:"accept"`
+				} `json:"other"`
+			} `json:"paymentMethods"`
+			Notes string `json:"notes"`
+		} `json:"evStationDetails,omitempty"`
+		LastUpdateTimestamp time.Time `json:"lastUpdateTimestamp,omitempty"`
+		TimeZone            string    `json:"timeZone,omitempty"`
+		Name                string    `json:"name,omitempty"`
 	} `json:"items"`
 }
 
@@ -122,8 +233,6 @@ func getEVChargePoints(lat, lng, limit string) (*EVChargingStation, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to read API response body: %w", err)
 	}
-
-	//fmt.Println("Raw JSON Response:", string(body))
 
 	var evChargePoints EVChargingStation
 	err = json.Unmarshal(body, &evChargePoints)
