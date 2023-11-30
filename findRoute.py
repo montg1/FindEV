d1bb96a291fd81af910c0f514c07061bb94ce506
Kajee <poomrapee.kho@gmail.com>
2023-11-30 17:09:03 +0700

    last version is cp2.py

--- findRoute.py
+++ findRoute.py
@@ -1,56 +1,90 @@
 import pandas as pd
-from geopy.distance import geodesic
+import folium
 import networkx as nx
-import matplotlib.pyplot as plt
+import heapq
+from bs4 import BeautifulSoup
+import re
+import sys
 
-def calculate_distance(coord1, coord2):
-    return geodesic(coord1, coord2).kilometers
+def dijkstra(graph, start, goal):
+    queue = [(0, start, [])]
+    visited = set()
 
-def create_graph(df):
-    G = nx.Graph()
+    while queue:
+        (cost, node, path) = heapq.heappop(queue)
+        if node not in visited:
+            visited.add(node)
+            path = path + [node]
 
-    for i, row in df.iterrows():
-        G.add_node(i, pos=(row['Latitude'], row['Longitude']))
+            if node == goal:
+                return path
 
-    for i in range(len(df)):
-        for j in range(i + 1, len(df)):
-            distance = calculate_distance((df.iloc[i]['Latitude'], df.iloc[i]['Longitude']),
-                                          (df.iloc[j]['Latitude'], df.iloc[j]['Longitude']))
-            G.add_edge(i, j, weight=distance)
+            for next_node in graph[node]:
+                if next_node not in visited:
+                    heapq.heappush(queue, (cost + graph[node][next_node]['weight'], next_node, path))
 
-    return G
+    return None
 
-def find_best_route(graph, start, goal):
-    path = nx.shortest_path(graph, source=start, target=goal, weight='weight')
-    return path
+# Load CSV data into a Pandas DataFrame
+df = pd.read_csv('Wales.csv', header=None, names=['ID', 'Location', 'City', 'Unused', 'Postal Code', 'County', 'Unused2', 'Country', 'Latitude', 'Longitude'])
 
-def main():
-    # Load CSV file
-    csv_file = 'Wales.csv'
-    df = pd.read_csv(csv_file)
+# Create a NetworkX graph using latitude and longitude as nodes and distance as weights
+G = nx.Graph()
 
-    # Get user input for start and goal coordinates
-    start_latitude = float(input("Enter current latitude: "))
-    start_longitude = float(input("Enter current longitude: "))
-    goal_latitude = float(input("Enter goal latitude: "))
-    goal_longitude = float(input("Enter goal longitude: "))
+# Load the path_map.html and extract the edges
+path_map = folium.Map(location=[52.1307, -3.7837], zoom_start=8)
+path_map.save('path_map.html')  # Make sure path_map.html is saved before running this script
 
-    # Find nearest nodes in the graph to the entered coordinates
-    start_node = ((df['Latitude'] - start_latitude)**2 + (df['Longitude'] - start_longitude)**2).idxmin()
-    goal_node = ((df['Latitude'] - goal_latitude)**2 + (df['Longitude'] - goal_longitude)**2).idxmin()
+with open('path_map.html', 'r') as f:
+    path_map_html = f.read()
 
-    # Create graph and find the best route
-    graph = create_graph(df)
-    best_route = find_best_route(graph, start=start_node, goal=goal_node)
+# Print the HTML content for inspection
+print(path_map_html)
 
-    # Print the best route
-    print(f"Best route: {best_route}")
+soup = BeautifulSoup(path_map_html, 'html.parser')
 
-    # Plot the graph with the best route
-    pos = nx.get_node_attributes(graph, 'pos')
-    nx.draw(graph, pos, with_labels=True, font_weight='bold')
-    nx.draw_networkx_edges(graph, pos, edgelist=[(best_route[i], best_route[i + 1]) for i in range(len(best_route) - 1)], edge_color='r', width=2)
-    plt.show()
+# Find the JavaScript code that adds the polyline
+script_pattern = re.compile(r'addLatLng\((.*?), (.*?)\);')
+matches = script_pattern.findall(path_map_html)
 
-if __name__ == "__main__":
-    main()
+if matches:
+    polyline = matches
+else:
+    print("Error: Unable to find polyline information in JavaScript code.")
+    sys.exit(1)
+
+
+# Add nodes and edges only for the paths in path_map.html
+for match in re.finditer(r'addLatLng\((.*?), (.*?)\);', polyline):
+    loc = tuple(map(float, match.groups()))
+    G.add_node(loc, location=df[(df['Latitude'] == loc[0]) & (df['Longitude'] == loc[1])]['Location'].values[0])
+
+for i in range(len(df)):
+    for j in range(i + 1, len(df)):
+        loc1 = (df.at[i, 'Latitude'], df.at[i, 'Longitude'])
+        loc2 = (df.at[j, 'Latitude'], df.at[j, 'Longitude'])
+
+        if G.has_node(loc1) and G.has_node(loc2):
+            distance = ((loc1[0] - loc2[0]) ** 2 + (loc1[1] - loc2[1]) ** 2) ** 0.5
+            G.add_edge(loc1, loc2, weight=distance)
+
+# Get user input for current and goal locations
+current_location = tuple(map(float, input("Enter the current location (latitude,longitude): ").split(',')))
+goal_location = tuple(map(float, input("Enter the goal location (latitude,longitude): ").split(',')))
+
+# Find the path using Dijkstra's algorithm
+path = dijkstra(G, current_location, goal_location)
+
+# Create a Folium map centered around Wales
+map_wales_dijkstra = folium.Map(location=[52.1307, -3.7837], zoom_start=8)
+
+# Add markers for each location
+for index, row in df.iterrows():
+    folium.Marker([row['Latitude'], row['Longitude']], popup=row['Location']).add_to(map_wales_dijkstra)
+
+# Add a path using the Dijkstra path
+if path:
+    folium.PolyLine(locations=path, color='red').add_to(map_wales_dijkstra)
+
+# Save the map as an HTML file
+map_wales_dijkstra.save('path_map_dijkstra.html')
